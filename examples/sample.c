e8c60e5bad5779af767d3e53deff196973619159
Prajwalkadam29 <160503760+Prajwalkadam29@users.noreply.github.com>
2025-10-28 00:06:42 +0530

    Create sample.c

---
+++ examples/sample.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+struct Point {
+    int x;
+    int y;
+};
+
+void move(struct Point *p, int dx, int dy) {
+    p->x += dx;
+    p->y += dy;
+}
+
+int main() {
+    struct Point p = {0, 0};
+    move(&p, 3, 4);
+    printf("%d %d\n", p.x, p.y);
+    return 0;
+}
